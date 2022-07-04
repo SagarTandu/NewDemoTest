b9866fdb90eb4e97a7b09bdef298dd008cc49dcb
Sagar  Tandu <sagar.tandu@gmail.com>
2022-07-04 21:03:50 +0530

    updated7.0

--- src/test/java/SF/test/login.java
+++ src/test/java/SF/test/login.java
@@ -28,7 +28,7 @@ public void initialize() {
 @Test (priority=0)
 public void blank() {
 	
-	System.setProperty("webdriver.chrome.driver", "C://Users//sagar.tandu//Desktop//chromedriver_win32//chromedriver.exe");
+	 System.setProperty("webdriver.chrome.driver", "C:/Users/sagar.tandu/Desktop/chromedriver_win32/chromedriver.exe");
 	driver=new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.get(url);
