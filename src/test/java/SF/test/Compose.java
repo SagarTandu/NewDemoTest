0083c045619cb2a3db41a3a16ac90a738af4f69a
Sagar  Tandu <sagar.tandu@gmail.com>
2022-07-05 15:33:18 +0530

    updated/

--- src/test/java/SF/test/Compose.java
+++ src/test/java/SF/test/Compose.java
@@ -28,7 +28,7 @@ public class Compose {
 	public void browser() {
 		//ChromeOptions ch = new ChromeOptions();
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sagar.tandu\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Dummy/Automate/drivers/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Dummy\ Automate/drivers/chromedriver.exe");
 		driver=new ChromeDriver();
 		//WebDriverManager.chromedriver().setup();
 		driver.manage().window().maximize();
