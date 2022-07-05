75d1c2899140f66f31cbfd80e7c0b12dc42b9de5
Sagar  Tandu <sagar.tandu@gmail.com>
2022-07-05 15:36:42 +0530

    updated1

--- src/test/java/SF/test/login.java
+++ src/test/java/SF/test/login.java
@@ -23,7 +23,7 @@ public String url= "https://accounts.google.com/";
 public void initialize() {
 	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sagar.tandu\\Downloads\\chromedriver_win32\\chromedriver.exe");
 	//System.setProperty("webdriver.chrome.driver", "C://Users//sagar.tandu//eclipse-workspace//test//drivers//chromedriver");
-	System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Dummy\ Automate/drivers/chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Automate/drivers/chromedriver.exe");
 
 	driver = new ChromeDriver();
 	driver.manage().window().maximize();
