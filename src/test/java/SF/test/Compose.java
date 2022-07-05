a87e30ba793a3f264c764101643577f5d4847f1b
Sagar  Tandu <sagar.tandu@gmail.com>
2022-07-05 17:57:25 +0530

    head

--- src/test/java/SF/test/Compose.java
+++ src/test/java/SF/test/Compose.java
@@ -30,7 +30,7 @@ public class Compose {
 		ChromeOptions ch = new ChromeOptions();
 		//ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");		
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sagar.tandu\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--no-sandbox");		
+		//ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--no-sandbox");		
 		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Automate/drivers/chromedriver");
 		driver=new ChromeDriver(ch);
 		//WebDriverManager.chromedriver().setup();
