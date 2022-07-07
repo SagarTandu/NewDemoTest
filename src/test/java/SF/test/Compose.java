713be8c3b51afe739b12ed45065aef18999f8aa4
Sagar  Tandu <sagar.tandu@gmail.com>
2022-07-07 09:43:21 +0530

    updated1

--- src/test/java/SF/test/Compose.java
+++ src/test/java/SF/test/Compose.java
@@ -25,12 +25,10 @@ public class Compose {
 	@Test 
 	public void browser() {
 		ChromeOptions ch = new ChromeOptions();
-		//ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage");		
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\sagar.tandu\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		ch.addArguments("--headless", "--disable-gpu", "--window-size=1920,1080","--no-sandbox");		
 		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Automate/drivers/chromedriver");
 		driver=new ChromeDriver(ch);
-		//WebDriverManager.chromedriver().setup();
 		driver.manage().window().maximize();
 	    driver.navigate().to(url);
 	}
@@ -68,7 +66,7 @@ public class Compose {
  }
 	
 	
- @Test
+ //@Test
 	public void close() {
 		driver.quit();
 	}
